521bdbc6eb14c8a523fe4e640c9b07ce11f9c611
Mark Soper <masoper@cockroachlabs.com>
2022-07-06 23:19:18 -0400

    serverless cluster example

---
+++ examples/serverless/cockroach_serverless_cluster.tf
@@ -0,0 +1,24 @@
+terraform {
+  required_providers {
+    cockroach = {
+      source = "registry.terraform.io/hashicorp/cockroach"
+    }
+  }
+}
+
+provider "cockroach" {
+# export COCKROACH_API_KEY with the cockroach cloud API Key
+}
+
+resource "cockroach_cluster" "cockroach" {
+    name           = "cockroach-cluster"
+    cloud_provider = "GCP"
+    wait_for_cluster_ready = true
+    create_spec = {
+        serverless = {
+            regions = ["us-east1"]
+            spend_limit = 1
+        }
+    }
+}
+

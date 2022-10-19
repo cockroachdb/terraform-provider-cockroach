b0a2f4b2458d123fd9de93e6fb59e16e507dbf7a
Mark Soper <soper@cockroachlabs.com>
2022-10-19 12:56:52 -0400

    cleanup examples

--- examples/resources/cockroach_aws_private_endpoint_connection/cockroach_aws_private_endpoint_connection.tf
+++ examples/resources/cockroach_aws_private_endpoint_connection/cockroach_aws_private_endpoint_connection.tf
@@ -3,9 +3,9 @@ variable cluster_id {
 }
 
 resource "cockroach_aws_private_endpoint_connection" "cockroach" {
-    id = var.cluster_id
-    cloud_provider = "AWS"
-    region = "AWS region in which the endpoint was created"
-    endpoint_id = "endpoint id assigned by consumer AWS"
-    status = "ENDPOINT_AVAILABLE"
+    id              = var.cluster_id
+    cloud_provider  = "AWS"
+    region          = "AWS region in which the endpoint was created"
+    endpoint_id     = "endpoint id assigned by consumer AWS"
+    status          = "ENDPOINT_AVAILABLE"
 }

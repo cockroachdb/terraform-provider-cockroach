38468e3e857e67a7a03c368ed120c5e23652c42f
jenngeorge <jenn@cockroachlabs.com>
2023-02-17 17:11:48 -0800

    Add LogExportConfig resource
    Add a new LogExportConfig resource, which manages
    the log export configuration for a cluster.

---
+++ internal/provider/log_export_config_resource_test.go
@@ -0,0 +1,304 @@
+/*
+ Copyright 2022 The Cockroach Authors
+
+ Licensed under the Apache License, Version 2.0 (the "License");
+ you may not use this file except in compliance with the License.
+ You may obtain a copy of the License at
+
+     http://www.apache.org/licenses/LICENSE-2.0
+
+ Unless required by applicable law or agreed to in writing, software
+ distributed under the License is distributed on an "AS IS" BASIS,
+ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ See the License for the specific language governing permissions and
+ limitations under the License.
+*/
+
+package provider
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+	"testing"
+
+	"github.com/cockroachdb/cockroach-cloud-sdk-go/pkg/client"
+	mock_client "github.com/cockroachdb/terraform-provider-cockroach/mock"
+	"github.com/golang/mock/gomock"
+	"github.com/google/uuid"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/terraform"
+)
+
+// TestAccLogExportConfigResource attempts to create, check, and destroy
+// a real cluster. It will be skipped if TF_ACC isn't set.
+func TestAccLogExportConfigResource(t *testing.T) {
+	t.Skip("Skipping until we can either integrate the AWS provider " +
+		"or import a permanent test fixture.")
+	t.Parallel()
+	clusterName := fmt.Sprintf("tftest-logexport-%s", GenerateRandomString(4))
+	testCMEKResource(t, clusterName, false)
+}
+
+// TestIntegrationLogExportConfigResource attempts to create, check, and destroy
+// a cluster, but uses a mocked API service.
+func TestIntegrationLogExportConfigResource(t *testing.T) {
+	clusterName := fmt.Sprintf("tftest-logexport-%s", GenerateRandomString(4))
+	clusterID := uuid.Nil.String()
+	if os.Getenv(CockroachAPIKey) == "" {
+		os.Setenv(CockroachAPIKey, "fake")
+	}
+
+	ctrl := gomock.NewController(t)
+	s := mock_client.NewMockService(ctrl)
+	defer HookGlobal(&NewService, func(c *client.Client) client.Service {
+		return s
+	})()
+
+	cluster := &client.Cluster{
+		Id:               clusterID,
+		Name:             clusterName,
+		CockroachVersion: "v22.2.0",
+		Plan:             "DEDICATED",
+		CloudProvider:    "AWS",
+		State:            "CREATED",
+		Config: client.ClusterConfig{
+			Dedicated: &client.DedicatedHardwareConfig{
+				MachineType:    "m5.xlarge",
+				NumVirtualCpus: 4,
+				StorageGib:     35,
+				MemoryGib:      8,
+			},
+		},
+		Regions: []client.Region{
+			{
+				Name:      "us-east-1",
+				NodeCount: 3,
+			},
+		},
+	}
+
+	authPrincipal := "iam-role-arn"
+	configType, _ := client.NewLogExportTypeFromValue("AWS_CLOUDWATCH")
+	logName := "test"
+	region := "us-east-1"
+	trueBool := true
+	minLevel, _ := client.NewLogLevelFromValue("WARNING")
+	createdGroups := []client.LogExportGroup{
+		{LogName: "sql", Channels: []string{"SQL_SCHEMA", "SQL_EXEC"}, MinLevel: minLevel, Redact: &trueBool},
+	}
+	enabledStatus, _ := client.NewLogExportStatusFromValue("ENABLED")
+	createdLogExportClusterInfo := &client.LogExportClusterInfo{
+		ClusterId: &clusterID,
+		Spec: &client.LogExportClusterSpecification{
+			AuthPrincipal: &authPrincipal,
+			LogName:       &logName,
+			Groups:        &createdGroups,
+			Redact:        &trueBool,
+			Type:          configType,
+			Region:        &region,
+		},
+		Status: enabledStatus,
+	}
+
+	falseBool := false
+	updatedGroups := []client.LogExportGroup{
+		{LogName: "sql", Channels: []string{"SQL_EXEC"}, MinLevel: minLevel, Redact: &trueBool},
+		{LogName: "devops", Channels: []string{"OPS", "HEALTH", "STORAGE"}, MinLevel: minLevel, Redact: &falseBool},
+	}
+	updatedLogExportClusterInfo := &client.LogExportClusterInfo{
+		ClusterId: &clusterID,
+		Spec: &client.LogExportClusterSpecification{
+			AuthPrincipal: &authPrincipal,
+			LogName:       &logName,
+			Groups:        &updatedGroups,
+			Redact:        &falseBool,
+			Type:          configType,
+			Region:        &region,
+		},
+		Status: enabledStatus,
+	}
+
+	// Create
+	s.EXPECT().CreateCluster(gomock.Any(), gomock.Any()).
+		Return(cluster, nil, nil)
+	s.EXPECT().GetCluster(gomock.Any(), clusterID).
+		Return(cluster, &http.Response{Status: http.StatusText(http.StatusOK)}, nil).
+		Times(3)
+	s.EXPECT().EnableLogExport(gomock.Any(), clusterID,
+		&client.EnableLogExportRequest{
+			Groups:        &createdGroups,
+			AuthPrincipal: authPrincipal,
+			LogName:       logName,
+			Redact:        &trueBool,
+			Type:          *configType,
+			Region:        &region,
+		}).
+		Return(createdLogExportClusterInfo, nil, nil)
+	s.EXPECT().GetLogExportInfo(gomock.Any(), clusterID).
+		Return(createdLogExportClusterInfo, nil, nil).
+		Times(3)
+
+	// Update
+	s.EXPECT().GetCluster(gomock.Any(), clusterID).
+		Return(cluster, nil, nil).
+		Times(2)
+	s.EXPECT().GetLogExportInfo(gomock.Any(), clusterID).
+		Return(createdLogExportClusterInfo, nil, nil)
+	s.EXPECT().EnableLogExport(gomock.Any(), clusterID,
+		&client.EnableLogExportRequest{
+			AuthPrincipal: authPrincipal,
+			Type:          *configType,
+			LogName:       logName,
+			Redact:        &falseBool,
+			Groups:        &updatedGroups,
+			Region:        &region,
+		}).
+		Return(updatedLogExportClusterInfo, nil, nil)
+	s.EXPECT().GetLogExportInfo(gomock.Any(), clusterID).
+		Return(updatedLogExportClusterInfo, nil, nil).
+		Times(3)
+
+	// Delete
+	s.EXPECT().DeleteCluster(gomock.Any(), clusterID)
+	s.EXPECT().DeleteLogExport(gomock.Any(), clusterID)
+
+	testLogExportConfigResource(t, clusterName, true)
+}
+
+func testLogExportConfigResource(t *testing.T, clusterName string, useMock bool) {
+	var (
+		clusterResourceName         = "cockroach_cluster.test"
+		logExportConfigResourceName = "cockroach_log_export_config.test"
+	)
+
+	resource.Test(t, resource.TestCase{
+		IsUnitTest:               useMock,
+		PreCheck:                 func() { testAccPreCheck(t) },
+		ProtoV6ProviderFactories: testAccProtoV6ProviderFactories,
+		Steps: []resource.TestStep{
+			{
+				Config: getTestLogExportConfigResourceCreateConfig(clusterName),
+				Check: resource.ComposeTestCheckFunc(
+					testLogExportConfigExists(logExportConfigResourceName, clusterResourceName),
+					resource.TestCheckResourceAttr(logExportConfigResourceName, "redact", "true"),
+					resource.TestCheckResourceAttr(logExportConfigResourceName, "groups.#", "1"),
+					resource.TestCheckResourceAttr(logExportConfigResourceName, "groups.0.channels.#", "2"),
+				),
+			},
+			{
+				Config: getTestLogExportConfigResourceUpdateConfig(clusterName),
+				Check: resource.ComposeTestCheckFunc(
+					testLogExportConfigExists(logExportConfigResourceName, clusterResourceName),
+					resource.TestCheckResourceAttr(logExportConfigResourceName, "redact", "false"),
+					resource.TestCheckResourceAttr(logExportConfigResourceName, "groups.#", "2"),
+					resource.TestCheckResourceAttr(logExportConfigResourceName, "groups.0.channels.#", "1"),
+				),
+			},
+		},
+	})
+}
+
+func testLogExportConfigExists(resourceName, clusterResourceName string) resource.TestCheckFunc {
+	return func(s *terraform.State) error {
+		p := testAccProvider.(*provider)
+		p.service = NewService(cl)
+
+		rs, ok := s.RootModule().Resources[resourceName]
+		if !ok {
+			return fmt.Errorf("not found: %s", resourceName)
+		}
+		clusterRs, ok := s.RootModule().Resources[clusterResourceName]
+		if !ok {
+			return fmt.Errorf("not found: %s", clusterResourceName)
+		}
+
+		clusterID := clusterRs.Primary.Attributes["id"]
+		log.Printf("[DEBUG] clusterID: %s, name %s", clusterRs.Primary.Attributes["id"], clusterRs.Primary.Attributes["name"])
+
+		logExportConfigResponse, _, err := p.service.GetLogExportInfo(context.TODO(), clusterID)
+		if err == nil {
+			if logExportConfigResponse.Spec.GetLogName() == rs.Primary.Attributes["log_name"] {
+				return nil
+			}
+		}
+
+		return fmt.Errorf("log export config with log name %s does not exist", rs.Primary.Attributes["log_name"])
+	}
+}
+
+func getTestLogExportConfigResourceCreateConfig(name string) string {
+	return fmt.Sprintf(`
+resource "cockroach_cluster" "test" {
+  name           = "%s"
+  cloud_provider = "AWS"
+  dedicated = {
+    storage_gib = 35
+  	machine_type = "m5.xlarge"
+  }
+  regions = [{
+    name = "us-east-1"
+    node_count: 3
+  }]
+}
+
+resource "cockroach_log_export_config" "test" {
+  id             = cockroach_cluster.test.id
+  auth_principal = "iam-role-arn"
+  log_name       = "test"
+  type           = "AWS_CLOUDWATCH"
+  redact         = true
+  region 		 = "us-east-1"
+  groups = [
+    {
+      log_name = "sql",
+      channels = ["SQL_SCHEMA", "SQL_EXEC"],
+      min_level = "WARNING",
+	  redact = true
+    }
+  ]
+}
+`, name)
+}
+
+func getTestLogExportConfigResourceUpdateConfig(name string) string {
+	return fmt.Sprintf(`
+resource "cockroach_cluster" "test" {
+  name           = "%s"
+  cloud_provider = "AWS"
+  dedicated = {
+    storage_gib = 35
+  	machine_type = "m5.xlarge"
+  }
+  regions = [{
+    name = "us-east-1"
+    node_count: 3
+  }]
+}
+
+resource "cockroach_log_export_config" "test" {
+  id             = cockroach_cluster.test.id
+  auth_principal = "iam-role-arn"
+  log_name       = "test"
+  type           = "AWS_CLOUDWATCH"
+  redact         = false
+  region 		 = "us-east-1"
+  groups = [
+    {
+      log_name = "sql",
+      channels = ["SQL_EXEC"],
+      min_level = "WARNING",
+	  redact: true
+    },
+    {
+      log_name = "devops",
+      channels = ["OPS", "HEALTH", "STORAGE"],
+      min_level = "WARNING",
+	  redact = false
+    }
+  ]
+}
+`, name)
+}

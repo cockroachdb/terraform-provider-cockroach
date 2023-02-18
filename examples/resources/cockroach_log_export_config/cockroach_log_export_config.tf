65e2ffa3d24625fb460e4903971e3e2d051e0750
jenngeorge <jenn@cockroachlabs.com>
2023-02-17 17:11:48 -0800

    Add LogExportConfig resource
    Add a new LogExportConfig resource, which manages
    the log export configuration for a cluster.

---
+++ examples/resources/cockroach_log_export_config/cockroach_log_export_config.tf
@@ -0,0 +1,27 @@
+variable "cluster_id" {
+  type = string
+}
+
+variable "auth_principal" {
+  type = string
+}
+
+resource "cockroach_log_export_config" "example" {
+  id             = var.cluster_id
+  auth_principal = var.auth_principal
+  log_name       = "example"
+  type           = "GCP_CLOUD_LOGGING"
+  redact         = true
+  groups = [
+    {
+      log_name : "sql",
+      channels : ["SQL_SCHEMA", "SQL_EXEC"],
+      redact : false
+    },
+    {
+      log_name : "devops",
+      channels : ["OPS", "HEALTH", "STORAGE"]
+      min_level : "WARNING"
+    }
+  ]
+}

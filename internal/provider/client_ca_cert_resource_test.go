8c889d44a00bf00fbdf3b3d0aee91dd61e109a37
Jason Chen <jasonc@cockroachlabs.com>
2023-03-21 15:37:39 -0700

    add acceptance test

--- internal/provider/client_ca_cert_resource_test.go
+++ internal/provider/client_ca_cert_resource_test.go
@@ -29,6 +29,20 @@ import (
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
 )
 
+// TestAccClientCACertResource attempts to create, check, and destroy
+// a real cluster and client CA certs. It will be skipped if TF_ACC isn't set.
+func TestAccClientCACertResource(t *testing.T) {
+	t.Parallel()
+	clusterName := fmt.Sprintf("tftest-clcacert-%s", GenerateRandomString(3))
+
+	// test cert with CN=test1
+	cert1 := "-----BEGIN CERTIFICATE-----\\nMIIDgTCCAmigAwIBAgIBADANBgkqhkiG9w0BAQ0FADBaMQswCQYDVQQGEwJ1czEL\\nMAkGA1UECAwCV0ExDjAMBgNVBAoMBXRlc3QxMQ4wDAYDVQQDDAV0ZXN0MTEOMAwG\\nA1UEBwwFdGVzdDExDjAMBgNVBAsMBXRlc3QxMB4XDTIzMDMyMTE5MDczN1oXDTI2\\nMDMyMDE5MDczN1owWjELMAkGA1UEBhMCdXMxCzAJBgNVBAgMAldBMQ4wDAYDVQQK\\nDAV0ZXN0MTEOMAwGA1UEAwwFdGVzdDExDjAMBgNVBAcMBXRlc3QxMQ4wDAYDVQQL\\nDAV0ZXN0MTCCASMwDQYJKoZIhvcNAQEBBQADggEQADCCAQsCggECAL8DDMgY6Vck\\nL1VfzXRXsGVYIdq/NQdVLUAUB/7WCd6/lVGvCMn/iR1uYUSDhTvW2CB4NqbFBE9Y\\nBwS5OZMqficezfR8xxRDsIIqjSlpWzE0j7uROyCFxXpt9PyIgTVX2Hj8xov8Y7aT\\nS5yddCURxjTC5/Fllwizn7PcJAqOcpzlqddHUXvcGKrEljyAf6fbpcCug4tR3Fq3\\ngf5rfwm1YR8w+GyFRiWy3B27oIftEEZ4GaN/J9+d5MZGS00PemilcQ3HyD20k7VU\\nbTylkYoDbTrt3ivK1SllkmOHtQ8eBZCF3S9kpctnF9ohy6k2F16wdLQ/XcGhMG5d\\nJ9eRWAXfy2EpAgMBAAGjUDBOMB0GA1UdDgQWBBR+mcFGBlCHOQe3H49JSdZMrqqS\\nnDAfBgNVHSMEGDAWgBR+mcFGBlCHOQe3H49JSdZMrqqSnDAMBgNVHRMEBTADAQH/\\nMA0GCSqGSIb3DQEBDQUAA4IBAgAZwBCWrvwxTnJ/3LtE8yG5tfhjoXZg3q19ZrNh\\nza0fElnvxONgNtLHjjNCWaf3er9EGW/Ge2NWuMGHjTwRwG9VzFETcnDrZVLf6uF7\\noriyzsno0Ob5VZkgeyFw23l/nEzGYtVM549Fz65trzyASOtqi1AH95xTd6fh3goq\\nd9ivSkP1FzGr4fwz8k9LQhE8YASgH6mZ5dO3ZNQjYzGsU7T5MIK/TYoyS4DDKQMM\\nGbhUuJXSoTsHrs9Jma6mGoBQaxJet6RfTk1+fDnCl9OdlS66Ez3V7KsLJ4Lzzi8S\\nJh5teJQymchzWt4Imn7VSHqedIF9psNaJ/ucHVaJAzkKBJGTzA==\\n-----END CERTIFICATE-----"
+	// test cert with CN=test2
+	cert2 := "-----BEGIN CERTIFICATE-----\\nMIIDgTCCAmigAwIBAgIBADANBgkqhkiG9w0BAQ0FADBaMQswCQYDVQQGEwJ1czEL\\nMAkGA1UECAwCV0ExDjAMBgNVBAoMBXRlc3QyMQ4wDAYDVQQDDAV0ZXN0MjEOMAwG\\nA1UEBwwFdGVzdDIxDjAMBgNVBAsMBXRlc3QyMB4XDTIzMDMyMTE5MTI1MVoXDTI2\\nMDMyMDE5MTI1MVowWjELMAkGA1UEBhMCdXMxCzAJBgNVBAgMAldBMQ4wDAYDVQQK\\nDAV0ZXN0MjEOMAwGA1UEAwwFdGVzdDIxDjAMBgNVBAcMBXRlc3QyMQ4wDAYDVQQL\\nDAV0ZXN0MjCCASMwDQYJKoZIhvcNAQEBBQADggEQADCCAQsCggECAL2hDCIH8t0e\\nIEGZlA44ekUu94EAFREnCu6UFAMvxCJVa3rfpNFIKxBRQTf0NL2K0hp4imx8PbyT\\n761U1NO3eFzy/CzXzf3RazdxU619wf/5IYg0fFyD1JfGd+tsbQMILkM14nIFcoVb\\ni7bxUVV4w7U3YIUCi811j/tabjTRmzFs+v2TiKcRRjNbIHCkBNAC+wt+XFhbL6oa\\nAqlpcMw9geris/ovD4kKaTLgAa0pxXiUQcYwBEuZoKzNTf1Un8A534HoKsU7lCND\\n/sz5haiHd4znzQ1pyY1GDoNT97XM92y9c2o4niNSedlkp7vlwwLseBz232qBs9mm\\nWdON2UYP74ohAgMBAAGjUDBOMB0GA1UdDgQWBBQnjmzTCRtNbO3RkA1Iwlh/xzQW\\nATAfBgNVHSMEGDAWgBQnjmzTCRtNbO3RkA1Iwlh/xzQWATAMBgNVHRMEBTADAQH/\\nMA0GCSqGSIb3DQEBDQUAA4IBAgApDx+zeTO+oLKus2n3v8blrwpmgms6tAvsh2kX\\ndGTdJForrv8hFMfkaVfSls+QwP6aMaDntdhM+cl+0BbDrxr4UYp2q7c3fESSnd5f\\nx+J4QReyMa2mt5nUf3iWdM2VhM5Ie5zcsWysnakzHs6Vo2anHjeZvNZxcwopzVaa\\nxvaBTduhzzqnzQZtr3D2qhtsOOKf3m5NprLnuwZ8gWDjpXnaR38R/w/KMtPKQRum\\n/hRf5AJfC5UrbMbnhMmEuO9ODnct/ejabbU/fTNbo2jeAXZusGYdDxNXuhCloSwJ\\nEOogpErNxiaLV5JVrRNtb2DPkdJKkaEUbjr63f+T3BcH7H0pLw==\\n-----END CERTIFICATE-----"
+
+	testClientCACertResource(t, clusterName, cert1, cert2, false)
+}
+
 func TestIntegrationClientCACertResource(t *testing.T) {
 	clusterName := fmt.Sprintf("tftest-clcacert-%s", GenerateRandomString(3))
 	clusterId := uuid.Nil.String()
@@ -58,8 +72,8 @@ func TestIntegrationClientCACertResource(t *testing.T) {
 		},
 		Regions: []client.Region{
 			{
-				Name:      "us-central-1",
-				NodeCount: 3,
+				Name:      "us-west-2",
+				NodeCount: 1,
 			},
 		},
 	}
@@ -121,10 +135,10 @@ func TestIntegrationClientCACertResource(t *testing.T) {
 	s.EXPECT().DeleteClientCACert(gomock.Any(), clusterId) // CERT Delete()
 	s.EXPECT().DeleteCluster(gomock.Any(), clusterId)      // CLUSTER Delete()
 
-	testClientCACertResource(t, clusterName, clusterId, cert1, cert2, true)
+	testClientCACertResource(t, clusterName, cert1, cert2, true)
 }
 
-func testClientCACertResource(t *testing.T, clusterName string, clusterId string, cert1 string, cert2 string, useMock bool) {
+func testClientCACertResource(t *testing.T, clusterName string, cert1 string, cert2 string, useMock bool) {
 	clusterResourceName := "cockroach_cluster.test"
 	cliCACertResourceName := "cockroach_client_ca_cert.test"
 
@@ -134,19 +148,17 @@ func testClientCACertResource(t *testing.T, clusterName string, clusterId string
 		ProtoV6ProviderFactories: testAccProtoV6ProviderFactories,
 		Steps: []resource.TestStep{
 			{
-				Config: getTestClientCACertResourceCreateConfig(clusterName, cert1),
+				Config: getTestClientCACertResourceConfig(clusterName, cert1),
 				Check: resource.ComposeTestCheckFunc(
 					resource.TestCheckResourceAttr(clusterResourceName, "name", clusterName),
-					resource.TestCheckResourceAttr(cliCACertResourceName, "id", clusterId),
 					resource.TestCheckResourceAttr(cliCACertResourceName, "x509_pem_cert", cert1),
 					resource.TestCheckResourceAttr(cliCACertResourceName, "status", string(client.CLIENTCACERTSTATUS_IS_SET)),
 				),
 			},
 			{
-				Config: getTestClientCACertResourceUpdateConfig(clusterName, cert2),
+				Config: getTestClientCACertResourceConfig(clusterName, cert2),
 				Check: resource.ComposeTestCheckFunc(
 					resource.TestCheckResourceAttr(clusterResourceName, "name", clusterName),
-					resource.TestCheckResourceAttr(cliCACertResourceName, "id", clusterId),
 					resource.TestCheckResourceAttr(cliCACertResourceName, "x509_pem_cert", cert2),
 					resource.TestCheckResourceAttr(cliCACertResourceName, "status", string(client.CLIENTCACERTSTATUS_IS_SET)),
 				),
@@ -155,7 +167,7 @@ func testClientCACertResource(t *testing.T, clusterName string, clusterId string
 	})
 }
 
-func getTestClientCACertResourceCreateConfig(name string, cert string) string {
+func getTestClientCACertResourceConfig(name string, cert string) string {
 	return fmt.Sprintf(`
 resource "cockroach_cluster" "test" {
 	name           = "%s"
@@ -165,30 +177,8 @@ resource "cockroach_cluster" "test" {
 		machine_type = "m5.xlarge"
 	}
 	regions = [{
-		name = "us-central-1"
-		node_count: 3
-	}]
-}
-
-resource "cockroach_client_ca_cert" "test" {
-	id = cockroach_cluster.test.id
-	x509_pem_cert = "%s"
-}
-`, name, cert)
-}
-
-func getTestClientCACertResourceUpdateConfig(name string, cert string) string {
-	return fmt.Sprintf(`
-resource "cockroach_cluster" "test" {
-	name           = "%s"
-	cloud_provider = "AWS"
-	dedicated = {
-		storage_gib = 35
-		machine_type = "m5.xlarge"
-	}
-	regions = [{
-		name = "us-central-1"
-		node_count: 3
+		name = "us-west-2"
+		node_count: 1
 	}]
 }
 

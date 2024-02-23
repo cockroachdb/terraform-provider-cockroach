2178f900dd9dd86b6c8e13622aeb9c9701338fcc
Mark Soper <soper@cockroachlabs.com>
2024-02-23 16:48:44 -0500

    adding workflow for standard

---
+++ examples/workflows/cockroach_standard_cluster/main.tf
@@ -0,0 +1,89 @@
+
+variable "cluster_name" {
+  type     = string
+  nullable = false
+}
+
+variable "sql_user_name" {
+  type     = string
+  nullable = false
+}
+
+# Remember that even variables marked sensitive will show up
+# in the Terraform state file. Always follow best practices
+# when managing sensitive info.
+# https://developer.hashicorp.com/terraform/tutorials/configuration-language/sensitive-variables#sensitive-values-in-state
+variable "sql_user_password" {
+  type      = string
+  nullable  = false
+  sensitive = true
+}
+
+variable "provisioned_capacity" {
+  type     = number
+  nullable = false
+  default  = 3000
+}
+
+variable "cloud_provider" {
+  type     = string
+  nullable = false
+  default  = "GCP"
+}
+
+variable "cloud_provider_regions" {
+  type     = list(string)
+  nullable = false
+  default  = ["us-central1", "us-east1", "europe-west1"]
+}
+
+variable "primary_region" {
+  type    = string
+  nullable = false
+  default = "us-east1"
+}
+
+terraform {
+  required_providers {
+    cockroach = {
+      version = "2.0.0"
+      source = "registry.terraform.io/cockroachdb/cockroach"
+    }
+  }
+}
+provider "cockroach" {
+  # export COCKROACH_API_KEY with the cockroach cloud API Key
+  
+}
+
+resource "cockroach_cluster" "example" {
+  name           = var.cluster_name
+  cloud_provider = var.cloud_provider
+  serverless = {
+    usage_limits = {
+      provisioned_capacity = var.provisioned_capacity
+    }
+  }
+  regions = [
+    for r in var.cloud_provider_regions :
+      r == var.primary_region ?
+        { 
+          name = r
+          primary = true
+        } :
+        {
+          name = r
+        }
+    ]
+}
+
+resource "cockroach_sql_user" "example" {
+  name       = var.sql_user_name
+  password   = var.sql_user_password
+  cluster_id = cockroach_cluster.example.id
+}
+
+resource "cockroach_database" "example" {
+  name       = "example-database"
+  cluster_id = cockroach_cluster.example.id
+}

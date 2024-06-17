1eed491dbea9c50a38237f0c8d71245c4f450db9
marksoper <soper@cockroachlabs.com>
2024-06-17 17:16:12 -0400

    source of docs

--- internal/provider/private_endpoint_connection_resource.go
+++ internal/provider/private_endpoint_connection_resource.go
@@ -50,6 +50,19 @@ func (r *privateEndpointConnectionResource) Schema(
 	resp.Schema = schema.Schema{
 		MarkdownDescription: "Private endpoint connections allow customer applications to connect to a CockroachDB Cloud cluster without traversing the public internet. All application-database traffic remains within the cloud-provider network.",
 		Attributes: map[string]schema.Attribute{
+			"warning": schema.StringAttribute{
+				Computed: true,
+				MarkdownDescription: `## _Warning_: Use of *private endpoints* requires >=v1.7.6
+	If you intend to use this provider to provision [private endpoints](https://github.com/cockroachdb/terraform-provider-cockroach/blob/main/docs/resources/private_endpoint_connection.md):
+	- [AWS PrivateLink](https://www.cockroachlabs.com/docs/cockroachcloud/aws-privatelink)
+	- [GCP Private Service Connect](https://www.cockroachlabs.com/docs/cockroachcloud/connect-to-your-cluster#gcp-private-service-connect)
+	- [Azure Private Link](https://www.cockroachlabs.com/docs/cockroachcloud/cockroachdb-dedicated-on-azure)
+	
+	You must install/upgrade to [version 1.7.6](https://github.com/cockroachdb/terraform-provider-cockroach/releases/tag/v1.7.6) or [later](https://registry.terraform.io/providers/cockroachdb/cockroach/latest)`,
+				PlanModifiers: []planmodifier.String{
+					stringplanmodifier.UseStateForUnknown(),
+				},
+			},
 			"id": schema.StringAttribute{
 				Computed:            true,
 				MarkdownDescription: "Used with `terraform import`. Format is \"<cluster ID>:<endpoint ID>\".",

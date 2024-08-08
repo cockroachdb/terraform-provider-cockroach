a4d7e5baccd2ee70c6a6c10cf9e3803984611bb2
Andrew Kimball <andyk@cockroachlabs.com>
2024-08-08 10:32:33 -0700

    update examples for Cloud 2.0

--- examples/resources/cockroach_cluster/resource.tf
+++ examples/resources/cockroach_cluster/resource.tf
@@ -1,9 +1,10 @@
-resource "cockroach_cluster" "dedicated" {
-  name           = "cockroach-dedicated"
+resource "cockroach_cluster" "advanced" {
+  name           = "cockroach-advanced"
   cloud_provider = "GCP"
+  plan           = "ADVANCED"
   dedicated = {
     storage_gib  = 15
-    machine_type = "n1-standard-4"
+    machine_type = "n2-standard-4"
   }
   regions = [
     {
@@ -14,8 +15,8 @@ resource "cockroach_cluster" "dedicated" {
   delete_protection = true
 }
 
-resource "cockroach_cluster" "serverless" {
-  name           = "cockroach-serverless"
+resource "cockroach_cluster" "standard" {
+  name           = "cockroach-standard"
   cloud_provider = "GCP"
   plan           = "STANDARD"
   serverless = {
@@ -23,6 +24,19 @@ resource "cockroach_cluster" "serverless" {
       provisioned_virtual_cpus = 2
     }
   }
+  regions = [
+    {
+      name = "us-east1"
+    }
+  ]
+  delete_protection = false
+}
+
+resource "cockroach_cluster" "basic" {
+  name           = "cockroach-basic"
+  cloud_provider = "GCP"
+  plan           = "BASIC"
+  serverless     = {}
   regions = [
     {
       name = "us-east1"

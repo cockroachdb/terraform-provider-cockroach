0c368a594c64538e64c40d48e9147fbfee0c2acd
Christopher Fitzner <fitzner@cockroachlabs.com>
2024-12-06 12:23:26 -0800

    vendor: add int64validator
    This caused an update to the validation framework again. It has been
    pulled in a separate commit for clarity.

--- vendor/github.com/hashicorp/terraform-plugin-framework-validators/internal/configvalidator/conflicting.go
+++ vendor/github.com/hashicorp/terraform-plugin-framework-validators/internal/configvalidator/conflicting.go
@@ -11,6 +11,7 @@ import (
 	"github.com/hashicorp/terraform-plugin-framework/attr"
 	"github.com/hashicorp/terraform-plugin-framework/datasource"
 	"github.com/hashicorp/terraform-plugin-framework/diag"
+	"github.com/hashicorp/terraform-plugin-framework/ephemeral"
 	"github.com/hashicorp/terraform-plugin-framework/path"
 	"github.com/hashicorp/terraform-plugin-framework/provider"
 	"github.com/hashicorp/terraform-plugin-framework/resource"
@@ -43,6 +44,10 @@ func (v ConflictingValidator) ValidateProvider(ctx context.Context, req provider
 }
 
 func (v ConflictingValidator) ValidateResource(ctx context.Context, req resource.ValidateConfigRequest, resp *resource.ValidateConfigResponse) {
+	resp.Diagnostics = v.Validate(ctx, req.Config)
+}
+
+func (v ConflictingValidator) ValidateEphemeralResource(ctx context.Context, req ephemeral.ValidateConfigRequest, resp *ephemeral.ValidateConfigResponse) {
 	resp.Diagnostics = v.Validate(ctx, req.Config)
 }
 

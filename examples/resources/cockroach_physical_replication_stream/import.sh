dad1d309a4137f7c17ca8997451875719d1a5632
David Ding <ding@cockroachlabs.com>
2025-07-01 16:39:32 -0400

    Implement support for physical cluster replication streams
    This implements support for replication stream creation and failover.
    Deletion will come later.

---
+++ examples/resources/cockroach_physical_replication_stream/import.sh
@@ -0,0 +1,2 @@
+# format: <physical replication stream id>
+terraform import cockroach_physical_replication_stream.my_stream 370c7f34-72e8-4238-b474-13767a579fb3
---
+++ examples/workflows/cockroach_physical_replication_stream/main.tf
@@ -0,0 +1,111 @@
+terraform {
+  required_providers {
+    cockroach = {
+      source = "cockroachdb/cockroach"
+    }
+  }
+}
+
+provider "cockroach" {
+  # export COCKROACH_API_KEY with the cockroach cloud API Key
+}
+
+variable "cluster_name_1" {
+  type     = string
+  nullable = false
+}
+
+variable "cluster_name_2" {
+  type     = string
+  nullable = false
+}
+
+variable "cloud_provider" {
+  type     = string
+  nullable = false
+  default  = "GCP"
+}
+
+variable "cloud_provider_regions_1" {
+  type     = list(string)
+  nullable = false
+  default  = ["us-east1"]
+}
+
+variable "cloud_provider_regions_2" {
+  type     = list(string)
+  nullable = false
+  default  = ["us-east1"]
+}
+
+variable "cluster_node_count" {
+  type     = number
+  nullable = false
+  default  = 1
+}
+
+variable "storage_gib" {
+  type     = number
+  nullable = false
+  default  = 15
+}
+
+variable "num_virtual_cpus" {
+  type     = number
+  nullable = false
+  default  = 4
+}
+
+variable "cidr_range_1" {
+  type     = string
+  nullable = false
+}
+
+variable "cidr_range_2" {
+  type     = string
+  nullable = false
+}
+
+resource "cockroach_cluster" "pcr_example_1" {
+  name           = var.cluster_name_1
+  cloud_provider = var.cloud_provider
+  plan           = "ADVANCED"
+  dedicated = {
+    storage_gib      = var.storage_gib
+    num_virtual_cpus = var.num_virtual_cpus
+    cidr_range       = var.cidr_range_1
+  }
+  regions = [
+    for r in var.cloud_provider_regions_1 : {
+      name       = r,
+      node_count = var.cluster_node_count
+    }
+  ]
+}
+
+resource "cockroach_cluster" "pcr_example_2" {
+  name           = var.cluster_name_2
+  cloud_provider = var.cloud_provider
+  plan           = "ADVANCED"
+  dedicated = {
+    storage_gib      = var.storage_gib
+    num_virtual_cpus = var.num_virtual_cpus
+    cidr_range       = var.cidr_range_2
+  }
+  regions = [
+    for r in var.cloud_provider_regions_2 : {
+      name       = r,
+      node_count = var.cluster_node_count
+    }
+  ]
+}
+
+
+resource "cockroach_physical_replication_stream" "pcr_stream" {
+  primary_cluster_id = cockroach_cluster.pcr_example_1.id
+  standby_cluster_id = cockroach_cluster.pcr_example_2.id
+  # After a PCR stream is replicating, uncomment this field to perform
+  # failover. Alternatively, set failover_at.
+  # failover_immediately = true
+}
+
